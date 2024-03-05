f8117203e39ba79931e4e7e644b1512ea011a007
Lafa Codes <roshan.thapa1005@gmail.com>
2024-03-06 00:26:30 +1100

    first commit

---
+++ js/load-comonents.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $("#nav").load("/components/nav.html");
+  $("#header").load("/components/header.html");
+  $("#main").load("/pages/lafa.codes_.html");
+  $("#about").load("/pages/about-lafa-codes.html");
+  $("#work").load("/pages/projects-by-lafa-codes.html");
+  $("#contact").load("/pages/contact-lafa-codes.html");
+  $("footer").load("/components/footer.html");
+});
